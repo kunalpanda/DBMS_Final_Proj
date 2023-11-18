2bd32c6dbfca6f4bd793e217825a6f46eaf3f825
snasirh <snasirh09@gmail.com>
2023-11-18 02:53:34 -0500

    added login page

---
+++ pages/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../css/forms.css">
+</head>
+<body>
+    <div class="form-container">
+        <form action="login.php" method="post">
+            <h2>Login (NOT READY)</h2>
+            <input type="text" name="usernameoremail" placeholder="Username or Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
