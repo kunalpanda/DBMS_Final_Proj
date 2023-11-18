645ba1f9bf38059d8681c726e07dcf4a960e6ccf
snasirh <snasirh09@gmail.com>
2023-11-18 02:47:31 -0500

    Added home page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+    <title>Home</title>
+    </head>
+<body bgcolor="#abc4c2">
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="./pages/signup.php">Register</a>
+        <a href="./pages/login.php">Login</a>
+    </div>
+
+    <h1>main</h1>
+</body>
+</html>
